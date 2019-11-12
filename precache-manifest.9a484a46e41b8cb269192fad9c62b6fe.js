ec3a971af4e3a8d579f0440c89a11ec308c7d3e7
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-12 19:12:14 +0300

    Updates

---
+++ precache-manifest.9a484a46e41b8cb269192fad9c62b6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db9861e3f4b17436e9e5d5d923e2d41",
+    "url": "/redux_changing-clothes/index.html"
+  },
+  {
+    "revision": "cfc0dd72cc6885b07c3a",
+    "url": "/redux_changing-clothes/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "53d14a2ed087cd2f8263",
+    "url": "/redux_changing-clothes/static/js/2.877d32e1.chunk.js"
+  },
+  {
+    "revision": "cfc0dd72cc6885b07c3a",
+    "url": "/redux_changing-clothes/static/js/main.b79553da.chunk.js"
+  },
+  {
+    "revision": "6b83c9740d74572bfe33",
+    "url": "/redux_changing-clothes/static/js/runtime-main.7881db55.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/redux_changing-clothes/static/media/logo.25bf045c.svg"
+  }
+]);
